9be418a9363005b112fcab6fba569da336268251
L4m4w <l4p41@yandex.com>
2025-05-28 22:37:54 +0300

    registration page tests

--- tests/e2e/web/test_login_page.py
+++ tests/e2e/web/test_login_page.py
@@ -2,5 +2,5 @@ from utils.utils_pages.utils_account import login_ui
 from pages.application import app
 from configs.settings import user_data
 
-def test_1():
+def test_create_repository():
     login_ui(user_data.email, user_data.password)
