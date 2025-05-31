50da46b522e26c70bdb2aedc82e6d2886d1bba37
L4m4w <l4p41@yandex.com>
2025-06-01 00:09:01 +0300

    registration page tests

--- pages/create_repository_page.py
+++ pages/create_repository_page.py
@@ -6,5 +6,29 @@ class CreateRepositoryPage(BasePage):
 
     PAGE_URL = '/new'
 
-    def enter_repository_name(self, text):
-        ...
+    # def __init__(self, repository_name):
+    #     self.repository_name = repository_name
+
+    def enter_repository_name(self, repository_name):
+        browser.element("#prc-components-Input-Ic-y8").element(by.id(":r5:"))
+        browser.element(by.id(":r5:")).send_keys(repository_name)
+        return self
+
+    def enter_description(self, description):
+        browser.element(by.id(":ra:")).send_keys(description)
+        return self
+
+    def choose_visibiltiy(self, visibility):
+        radio_button_selector = \
+            'input[name="visibilityGroup"][value="public"]' \
+                if visibility == 'public' \
+                else 'input[name="visibilityGroup"][value="private"]'
+        browser.element(
+            radio_button_selector
+        ).click()
+        return self
+
+    @property
+    def create_repository_button(self):
+        browser.element('button:contains("Create repository")')
+        return self
