ee976e9166030562c28d3d57ece3b96e86e2613b
L4m4w <l4p41@yandex.com>
2025-06-12 19:24:09 +0300

    Added data generator + 1st test + more stable selectors

--- tests/ui/web/pull_requests/test_pulls_page.py
+++ tests/ui/web/pull_requests/test_pulls_page.py
@@ -23,9 +23,23 @@ def test_branch_in_branch_table():
     cookies = get_auth_cookie(user_data.email, user_data.password)
     app.branch_page.with_cookies(cookies).open()
     # app.branch_page.get_user_branch_from_table('321')
-    branch = app.branch_page.get_user_branch_from_table()
+    # branch = app.branch_page.get_user_branch_from_table()
+    branch = browser.element("div[title='321']")
     branch_link = branch.element('..')
-    print(debug_element(branch_link))
+    # branch_link = branch_link.element('//a')
+    all_attributes = browser.driver.execute_script(
+        """
+        const elem = arguments[0];
+        const attributes = {};
+        for (let i = 0; i < elem.attributes.length; i++) {
+            attributes[elem.attributes[i].name] = elem.attributes[i].value;
+        }
+        return attributes;
+        """,
+        branch_link.locate()  # Получаем Selenium-WebElement
+    )
+    # print(debug_element(branch_link))
+    print(all_attributes)
     # branch_link = branch_link.element('//a')
     # branch_link = app.branch_page.branch_href_link('321')
     # all_attributes = browser.driver.execute_script(
@@ -41,7 +55,7 @@ def test_branch_in_branch_table():
     # )
     # print(all_attributes)
     # print(branch_link.)
-    # branch_link.should(have.css_property('href', "/evgne/Desiree-Leblanc/tree/321"))
+    branch_link.should(have.attribute('href', "https://github.com/evgne/Desiree-Leblanc/tree/321"))
     # href = branch_link.should(have.attribute("href"))
     # branch_link.should(have.attribute('href', "/evgne/Desiree-Leblanc/tree/321"))
 
