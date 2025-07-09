22da3cfd07d73b7bb4e6d1bc1890ecc1094f9714
L4m4w <l4p41@yandex.com>
2025-07-09 22:23:36 +0300

    rework env settings

--- configs/settings.py
+++ configs/settings.py
@@ -1,4 +1,4 @@
-from pydantic import SecretStr
+from pydantic import SecretStr, Field
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
@@ -13,18 +13,18 @@ def find_project_root():
 PROJECT_ROOT = find_project_root()
 
 class UserData(BaseSettings):
-    email : str
-    password : str
-    username : str
-    base_url : str
-    git_token : str
-    token: str
-    repository_name: str
-    browserstack_username: str
-    browserstack_access_key: str
+    email : str = Field(..., alias="USER_DATA_EMAIL")
+    password : str = Field(..., alias="USER_DATA_PASSWORD")
+    username : str = Field(..., alias="USER_DATA_USERNAME")
+    base_url : str = Field(..., alias="USER_DATA_BASE_URL")
+    git_token : str = Field(..., alias="USER_DATA_GIT_TOKEN")
+    token: str = Field(..., alias="USER_DATA_TOKEN")
+    repository_name: str = Field(..., alias="USER_DATA_REPOSITORY_NAME")
+    browserstack_username: str = Field(..., alias="USER_DATA_BROWSERSTACK_USERNAME")
+    browserstack_access_key: str = Field(..., alias="USER_DATA_BROWSERSTACK_ACCESS_KEY")
 
     class Config:
-        env_file = PROJECT_ROOT / ".env.local"
+        env_file = PROJECT_ROOT / ".env.local" if Path(".env.local").exists() else None
         env_prefix = "USER_DATA_"
 
 class TestSettings(BaseSettings):
